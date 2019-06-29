4a4fd5dbd3c51ba3c6f91495164c480b9b2518a4
markamaze <mark.amaze@gmail.com>
2019-06-28 20:23:08 -0700

    update app.use changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT
 
-app.use('/dist', express.static('dist'))
+app.use(express.static(path.join(__dirname + '/dist/'))
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname + "/dist/")))
 
