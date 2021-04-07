21c39e0c7783f147f9b740339281a56904d51313
mark <mark.amaze@gmail.com>
2021-04-07 04:56:02 -0700

    rebuilt with new template

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.PORT
+// const port = process.env.PORT
+const port = 3000
 
-app.use(express.static(path.join(__dirname + '/dist/')))
+app.use(express.static(path.join(__dirname + '/')))
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname + "/dist/")))
+  res.sendFile(path.join(__dirname + "/")))
 
 app.listen(port, () => console.log(`listening on port ${port}`))
